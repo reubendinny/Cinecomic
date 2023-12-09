a6389ef4b5bdc7273aba2140345cca32f97e56f3
Reuben <reubendinny@gmail.com>
2023-12-09 13:43:44 +0530

    extracted features

--- keyframe.py
+++ keyframe.py
@@ -1,43 +1,53 @@
 import torch
+from torchvision import transforms
+from PIL import Image
 import numpy as np
 
-googlenet = torch.hub.load('pytorch/vision:v0.10.0', 'googlenet', pretrained=True)
-print(googlenet)
+def _get_features(frames, gpu=True, batch_size=1):
+    # Load pre-trained GoogLeNet model
+    googlenet = torch.hub.load('pytorch/vision:v0.10.0', 'googlenet', pretrained=True)
+    
+    # Remove the classification layer (last layer) to obtain features
+    googlenet = torch.nn.Sequential(*(list(googlenet.children())[:-1]))
 
-googlenet = torch.nn.Sequential(*(list(googlenet.children())[:-1]))
-# print(googlenet)
+    # Set the model to evaluation mode
+    googlenet.eval()
+
+    # Initialize a list to store the features
+    features = []
+
+    # Image preprocessing pipeline
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    # Iterate through frames
+    for frame_path in frames:
+        # Load and preprocess the frame
+        input_image = Image.open(frame_path)
+        input_tensor = preprocess(input_image)
+        input_batch = input_tensor.unsqueeze(0)  # Add batch dimension
+
+        # Move the input and model to GPU if available
+        if gpu:
+            input_batch = input_batch.to('cuda')
+            googlenet.to('cuda')
+
+        # Perform feature extraction
+        with torch.no_grad():
+            output = googlenet(input_batch)
+
+        # Append the features to the list
+        features.append(output.squeeze().cpu().numpy())
+
+    # Convert the list of features to a NumPy array
+    features = np.array(features)
+
+    return features.astype(np.float32)
 
 
-# sample execution (requires torchvision)
-from PIL import Image
-
-from torchvision import transforms
-input_image = Image.open("video/ironman.jpg")
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-input_tensor = preprocess(input_image)
-input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
-
-# move the input and model to GPU for speed if available
-if torch.cuda.is_available():
-    input_batch = input_batch.to('cuda')
-    googlenet.to('cuda')
-
-with torch.no_grad():
-    output = googlenet(input_batch)
-# Tensor of shape 1000, with confidence scores over ImageNet's 1000 classes
-print(type(output[0]))
-
-# numpy_array = output.numpy()
-
-# Convert the NumPy array to np.float32
-# numpy_array_float32 = output.astype(np.float32)
-# # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-# probabilities = torch.nn.functional.softmax(output[0], dim=0)
-# print(probabilities)
-numpy_array_float32_gpu = output.to('cpu', torch.float32).numpy()
-print(type(numpy_array_float32_gpu))
+frames = ["video/ironman.jpg"]
+print(_get_features(frames))
