841bfcd21478b377af0859b712fc5ce31be7cb9b
Reuben <reubendinny@gmail.com>
2024-02-12 15:45:59 +0530

    layout half

--- layout/page.py
+++ layout/page.py
@@ -1,3 +1,7 @@
+import os
+import random
+
+
 def parse_css_file(css_file):
     css_dict = {}
     current_selector = None
@@ -31,9 +35,73 @@ def write_css_file(css_dict, output_file):
             f.write("}\n")
 
 
-css_file = 'page.css'  # Replace 'styles.css' with your CSS file path
+def set_background_image(key, image_path):  
+    url_str =   f'url("images/{image_path}");'
+    css_dict[key]['background-image'] =  url_str
+
+lis = ['row' , 'column']
+
+def get_files_in_folder(folder_path):
+    file_dicts = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_dicts.append({"name": file , 'rank' :  random.randint(1, 3) , 'span' :random.choice(lis) })
+    return file_dicts
+
+
+
+
+
+css_file = 'template.css'  # Replace 'styles.css' with your CSS file path
 css_dict = parse_css_file(css_file)
 print(css_dict)
+
+
+# css_dict['#_3']['background-color'] = 'red'
+
+
+folder_path = 'images' # Specify the folder path
+images = get_files_in_folder(folder_path)
+print(images)
+
+
+
+# print(len(images))
+
+# delete = 9 - len(images)
+
+
+
+i=1
+total_rank = 0
+
+for image in images:
+
+    total_rank = total_rank + image['rank']
+    print(total_rank)
+    if(total_rank >=6):
+        break
+
+    set_background_image( f'#_{i}', image['name'] )
+
+    css_dict[f'#_{i}']['grid-'+ image['span']] = 'span ' + str(image['rank'])   #Trying to assign this: grid-column: span 2;
+
+    i = i+1
+   
+
+# print(total_rank)
+
+# New logic
+
+grid = [[000],[000],[000]]
+
+
+
+
+
+
+
 
 
 
