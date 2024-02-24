cb91f93b04dec0bc2a89fc7d3c8cac9e00f37f87
Raphael <prince4raphael@gmail.com>
2024-02-25 01:33:22 +0530

    basic comic effect

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # from backend.subtitles.subs import get_subtitles
-# from backend.keyframes.keyframes import generate_keyframes
+from backend.keyframes.keyframes import generate_keyframes
 from backend.panel_layout.layout_gen import generate_layout
+from backend.cartoonize.cartoonize import style_frames
 
 video = 'video/harry.mp4'
 # get_subtitles(video)
 
-# generate_keyframes(video)
+generate_keyframes(video)
 
 generate_layout()
-
+style_frames()
