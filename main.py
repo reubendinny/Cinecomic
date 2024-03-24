8d115f811eaf43ed6ef2e5777e51db7c7c762a46
Raphael <prince4raphael@gmail.com>
2024-03-24 14:26:34 +0530

    Basic ready, implentation has problems

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # from backend.subtitles.subs import get_subtitles
-from backend.keyframes.keyframes import generate_keyframes, black_bar_crop
-from backend.panel_layout.layout_gen import generate_layout
-from backend.cartoonize.cartoonize import style_frames
+# from backend.keyframes.keyframes import generate_keyframes, black_bar_crop
+# from backend.panel_layout.layout_gen import generate_layout
+# from backend.cartoonize.cartoonize import style_frames
+from backend.speech_bubble.lip_detection import get_lips
 
 video = 'video/harry.mp4'
 # get_subtitles(video)
 
-generate_keyframes(video)
-black_bar_crop()
+# generate_keyframes(video)
+# black_bar_crop()
 
-generate_layout()
-style_frames()
+# generate_layout()
+# style_frames()
+get_lips(video)
