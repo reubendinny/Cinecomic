62f32a78509507972de4b5982afa97a1baa8a39e
Reuben <reubendinny@gmail.com>
2024-04-02 12:31:09 +0530

    dialog inserted in bubble

--- backend/speech_bubble/bubble_place.js
+++ backend/speech_bubble/bubble_place.js
@@ -1,4 +1,5 @@
 function placeDialogs() {
+    var i = 0;
     const gridItems = document.querySelectorAll('.grid-item'); // Select all grid items
 
     gridItems.forEach(item => {
@@ -6,12 +7,12 @@ function placeDialogs() {
         dialog.classList.add('dialog'); // Add the 'dialog' class to the div
         dialog.style.transform = "translate(45px, 45px)"
        
-        dialog.innerHTML = 'Hello, this is testing. Will this work?'; // Add content to the dialog
-
+        dialog.innerHTML = bubble[i]['dialog']; // Add content to the dialog
+        i = i +1;   
         const tail= document.createElement('div'); // Create a new div for the tail
         tail.classList.add('tail'); // Add the 'tail' class to the div
         dialog.appendChild(tail); // Append the tail div to the dialog div
-        tail.style.transform = "translate(45px, 45px) rotate(0deg)"
+        tail.style.transform = `translate(45px, 45px) rotate(${bubble[i]['tail_deg']}deg)`
 
 
         item.appendChild(dialog); // Append the dialog div to the current grid item
