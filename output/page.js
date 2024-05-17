7bb1e307c16945f14cb81e5161cfcade1e5c3de0
Reuben <reubendinny@gmail.com>
2024-05-17 21:09:47 +0530

    working

---
+++ output/page.js
@@ -0,0 +1,472 @@
+var pages = [
+    {
+        "panels": [
+            {
+                "image": "frame001",
+                "row_span": 2,
+                "col_span": 1
+            },
+            {
+                "image": "frame002",
+                "row_span": 1,
+                "col_span": 3
+            },
+            {
+                "image": "frame003",
+                "row_span": 1,
+                "col_span": 2
+            },
+            {
+                "image": "frame004",
+                "row_span": 2,
+                "col_span": 1
+            },
+            {
+                "image": "frame005",
+                "row_span": 1,
+                "col_span": 2
+            },
+            {
+                "image": "frame006",
+                "row_span": 1,
+                "col_span": 1
+            }
+        ],
+        "bubbles": [
+            {
+                "dialog": "Father, I wish to confess.",
+                "emotion": "normal",
+                "bubble_offset_x": 43.75,
+                "bubble_offset_y": 30.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I wish to confess.",
+                "emotion": "normal",
+                "bubble_offset_x": 30.0,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "You wish to confess?",
+                "emotion": "jagged",
+                "bubble_offset_x": 302.5,
+                "bubble_offset_y": 257.6666666666667,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I saved you.",
+                "emotion": "normal",
+                "bubble_offset_x": 43.75,
+                "bubble_offset_y": 624.3333333333334,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I saved this city and all your worthless lives.",
+                "emotion": "normal",
+                "bubble_offset_x": 20.0,
+                "bubble_offset_y": 257.6666666666667,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I should have let Stannis kill you all.",
+                "emotion": "normal",
+                "bubble_offset_x": 10.0,
+                "bubble_offset_y": 244.44444444444446,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            }
+        ]
+    },
+    {
+        "panels": [
+            {
+                "image": "frame007",
+                "row_span": 1,
+                "col_span": 1
+            },
+            {
+                "image": "frame008",
+                "row_span": 1,
+                "col_span": 2
+            },
+            {
+                "image": "frame009",
+                "row_span": 2,
+                "col_span": 1
+            },
+            {
+                "image": "frame010",
+                "row_span": 1,
+                "col_span": 3
+            },
+            {
+                "image": "frame011",
+                "row_span": 1,
+                "col_span": 2
+            },
+            {
+                "image": "frame012",
+                "row_span": 1,
+                "col_span": 2
+            }
+        ],
+        "bubbles": [
+            {
+                "dialog": "Sirian.",
+                "emotion": "normal",
+                "bubble_offset_x": 43.75,
+                "bubble_offset_y": 257.6666666666667,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "Do you wish to confess?",
+                "emotion": "jagged",
+                "bubble_offset_x": 302.5,
+                "bubble_offset_y": 257.6666666666667,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "Yes, Father.",
+                "emotion": "normal",
+                "bubble_offset_x": 10.0,
+                "bubble_offset_y": 30.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I'm guilty.",
+                "emotion": "normal",
+                "bubble_offset_x": 561.25,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "Guilty.",
+                "emotion": "normal",
+                "bubble_offset_x": 302.5,
+                "bubble_offset_y": 15.0,
+                "tail_deg": 120.64197608219571,
+                "tail_offset_x": -40.77375013058261,
+                "tail_offset_y": 68.82950893540367
+            },
+            {
+                "dialog": "Is that what you want to hear?",
+                "emotion": "jagged",
+                "bubble_offset_x": 302.5,
+                "bubble_offset_y": 257.6666666666667,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            }
+        ]
+    },
+    {
+        "panels": [
+            {
+                "image": "frame013",
+                "row_span": 1,
+                "col_span": 1
+            },
+            {
+                "image": "frame014",
+                "row_span": 1,
+                "col_span": 2
+            },
+            {
+                "image": "frame015",
+                "row_span": 2,
+                "col_span": 1
+            },
+            {
+                "image": "frame016",
+                "row_span": 1,
+                "col_span": 3
+            },
+            {
+                "image": "frame017",
+                "row_span": 1,
+                "col_span": 2
+            },
+            {
+                "image": "frame018",
+                "row_span": 1,
+                "col_span": 2
+            }
+        ],
+        "bubbles": [
+            {
+                "dialog": "You admit you poisoned the king.",
+                "emotion": "normal",
+                "bubble_offset_x": 43.75,
+                "bubble_offset_y": 257.6666666666667,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "No.",
+                "emotion": "normal",
+                "bubble_offset_x": 302.5,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "Of that I'm innocent.",
+                "emotion": "normal",
+                "bubble_offset_x": 10.0,
+                "bubble_offset_y": 624.3333333333334,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I'm guilty of a far more monstrous crime.",
+                "emotion": "normal",
+                "bubble_offset_x": 561.25,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I'm guilty of being a dwarf.",
+                "emotion": "normal",
+                "bubble_offset_x": 302.5,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "You're not on trial for being a dwarf.",
+                "emotion": "normal",
+                "bubble_offset_x": 302.5,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            }
+        ]
+    },
+    {
+        "panels": [
+            {
+                "image": "frame019",
+                "row_span": 2,
+                "col_span": 1
+            },
+            {
+                "image": "frame020",
+                "row_span": 1,
+                "col_span": 3
+            },
+            {
+                "image": "frame021",
+                "row_span": 1,
+                "col_span": 2
+            },
+            {
+                "image": "frame022",
+                "row_span": 2,
+                "col_span": 1
+            },
+            {
+                "image": "frame023",
+                "row_span": 1,
+                "col_span": 2
+            },
+            {
+                "image": "frame024",
+                "row_span": 1,
+                "col_span": 1
+            }
+        ],
+        "bubbles": [
+            {
+                "dialog": "Oh, yes I am.",
+                "emotion": "normal",
+                "bubble_offset_x": 43.75,
+                "bubble_offset_y": 30.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I've been on trial for that my entire life.",
+                "emotion": "normal",
+                "bubble_offset_x": 561.25,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "Have you nothing to say in your defense?",
+                "emotion": "normal",
+                "bubble_offset_x": 20.0,
+                "bubble_offset_y": 257.6666666666667,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "Nothing but this.",
+                "emotion": "normal",
+                "bubble_offset_x": 43.75,
+                "bubble_offset_y": 30.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I did not do it.",
+                "emotion": "normal",
+                "bubble_offset_x": 20.0,
+                "bubble_offset_y": 257.6666666666667,
+                "tail_deg": 34.406328627734936,
+                "tail_offset_x": 66.00408716749929,
+                "tail_offset_y": 45.20465105700026
+            },
+            {
+                "dialog": "I did not kill Joffrey, but I wish that I had.",
+                "emotion": "normal",
+                "bubble_offset_x": 10.0,
+                "bubble_offset_y": 257.6666666666667,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            }
+        ]
+    },
+    {
+        "panels": [
+            {
+                "image": "frame025",
+                "row_span": 1,
+                "col_span": 3
+            },
+            {
+                "image": "frame026",
+                "row_span": 1,
+                "col_span": 1
+            },
+            {
+                "image": "frame027",
+                "row_span": 2,
+                "col_span": 1
+            },
+            {
+                "image": "frame028",
+                "row_span": 1,
+                "col_span": 3
+            },
+            {
+                "image": "frame029",
+                "row_span": 1,
+                "col_span": 2
+            },
+            {
+                "image": "frame030",
+                "row_span": 1,
+                "col_span": 1
+            }
+        ],
+        "bubbles": [
+            {
+                "dialog": "Watching your vicious bastard die gave me more relief than a thousand lying whores.",
+                "emotion": "jagged",
+                "bubble_offset_x": 465.74999999999994,
+                "bubble_offset_y": 15.0,
+                "tail_deg": 47.31519869386365,
+                "tail_offset_x": 54.23717577894715,
+                "tail_offset_y": 58.8075570273378
+            },
+            {
+                "dialog": "I wish I was the monster you think I am.",
+                "emotion": "normal",
+                "bubble_offset_x": 43.75,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I wish I had enough poison for the whole pack of you.",
+                "emotion": "normal",
+                "bubble_offset_x": 43.75,
+                "bubble_offset_y": 624.3333333333334,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I would gladly give my life to watch you all swallow it.",
+                "emotion": "normal",
+                "bubble_offset_x": 30.0,
+                "bubble_offset_y": 15.0,
+                "tail_deg": 29.858765906840375,
+                "tail_offset_x": 69.38042171407884,
+                "tail_offset_y": 39.82909844292963
+            },
+            {
+                "dialog": "Sir Merrin.",
+                "emotion": "normal",
+                "bubble_offset_x": 73.37962962962962,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "Sir Merrin has caught the prisoner back to his cell.",
+                "emotion": "normal",
+                "bubble_offset_x": 10.0,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            }
+        ]
+    },
+    {
+        "panels": [
+            {
+                "image": "frame031",
+                "row_span": 1,
+                "col_span": 4
+            },
+            {
+                "image": "frame032",
+                "row_span": 1,
+                "col_span": 4
+            },
+            {
+                "image": "frame033",
+                "row_span": 1,
+                "col_span": 4
+            }
+        ],
+        "bubbles": [
+            {
+                "dialog": "I will not give my life for Joffrey's murder, and I know I'll get no justice here.",
+                "emotion": "normal",
+                "bubble_offset_x": 30.0,
+                "bubble_offset_y": 67.32234432234432,
+                "tail_deg": 20.485782741359582,
+                "tail_offset_x": 74.94072481287424,
+                "tail_offset_y": 27.997995723284493
+            },
+            {
+                "dialog": "So I will let the gods decide my fate.",
+                "emotion": "normal",
+                "bubble_offset_x": 30.0,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            },
+            {
+                "dialog": "I demand a trial by combat.",
+                "emotion": "normal",
+                "bubble_offset_x": 517.5,
+                "bubble_offset_y": 15.0,
+                "tail_offset_x": null,
+                "tail_offset_y": null
+            }
+        ]
+    }
+]
